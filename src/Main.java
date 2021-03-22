a65388a03853fd7d265bb555ce4b82f05094b384
”Buharin <black_cloak56@mail.ru>
2021-03-22 12:51:43 +0500

    clear phone number

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,15 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Введите номер телефона:");
         Scanner sc = new Scanner(System.in);
         String clear = sc.nextLine().replaceAll("[^0-9]", "").replaceFirst("[8]", "7");
-        if (clear.length() == 11) {
-            if (clear.length() - 1 == 7 || clear.length() - 1 == 8) {
-                System.out.println(clear);
-            } else System.out.println("Номер неверный.");
-        }
-        if (clear.length() == 10) {
+        String first = String.valueOf(clear.charAt(0));
+        if (clear.length() == 11 && first.equals("7")) {
+            System.out.println(clear + "пачиму");
+        }else if (clear.length() == 10) {
             System.out.println("7" + clear);
-        } else if (clear.length() < 10 || clear.length() > 11) {
-            System.out.println("Номер неверный.");
-        }
-
+        }else System.out.println("Номер неверный.");
     }
 }
